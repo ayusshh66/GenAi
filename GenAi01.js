222bfc2072a9a40aeeab6bd5c2ed0c073dc579e5
ayusshh66 <adonisjefrey66@gmail.com>
2026-04-01 23:00:02 +0530

    GenAi

---
+++ GenAi01.js
@@ -0,0 +1,40 @@
+import { GoogleGenAI } from "@google/genai";
+import readlineSync from 'readline-sync';
+
+
+const ai = new GoogleGenAI({
+  apiKey: ""
+});
+
+const History = []
+
+async function Chatting(userProblem) {
+    History.push({
+        role : 'user',
+        parts : [{text : userProblem}]
+    });
+
+
+  const response = await ai.models.generateContent({
+    model: "gemini-2.5-flash",
+    contents: History
+  });
+
+  History.push({
+        role : 'model',
+        parts : [{text : response.text}]
+    });
+
+
+  console.log(response.text);
+}
+
+async function main() {
+    const userProblem = readlineSync.question("Ask me you queries --->")
+    await Chatting(userProblem);
+    main();
+
+    
+}
+
+main().catch(console.error);
---
+++ GenAi02.js
@@ -0,0 +1,31 @@
+import { GoogleGenAI } from "@google/genai";
+import readlineSync from 'readline-sync';
+
+
+const ai = new GoogleGenAI({
+  apiKey: ""
+});
+
+
+
+  const chat = ai.chats.create({
+    model: "gemini-3-flash-preview",
+    history: [],
+  })
+
+
+async function main() {
+    const userProblem = readlineSync.question("Ask me you queries --->")
+    // await Chatting(userProblem);
+    const response = await chat.sendMessage({
+    message: userProblem,
+     });
+
+console.log(response.text) 
+
+    main();
+
+    
+}
+
+main().catch(console.error);
